56eb750b6bfc57ca0b4150ca09e1fd8cdf1e55aa
xy <614282119@qq.com>
2020-08-26 15:46:34 +0800

    支持webvpn，无需校园网环境

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 # BIT_Score_Inquiry
 
+2020/8/26更新：现可支持webvpn，不用校园网可以直接查询。
+## 依赖库
+- python3
+- bs4
+- requests
+- argparse
+
 ## 功能
 - 成绩有更新时提醒
 - 计算每个学期的加权平均分和总得加权平均分 ~~让您充满B TREE~~
 
-使用学号密码登陆之后进行网页访问，需要使用者输入学号以及密码，默认查询频率为60s，有新的成绩出现时，会显示课程名，分数，以及学分。
+## 参数说明
+1. --ID: 学号
+2. --password: 登录教务处的密码
+3. --freq: 非必须，默认为-1，如果设置为整数，则以该频率反复查询(用于第一时间得到出分消息)
 
-## TODO：
-- ~~学号密码登录~~
-- 短信通知功能
-- docker封装
+## 用法
+1. python getScore.py --ID xxxxx --password xxxxx 
 
+2. python getScore.py --ID xxxxx --password xxxxx --freq 60 
 
-## 运行
-python getScore.py --ID xxxxx --password xxxxx 
+## 效果截图
 
-
+<img src="https://gitee.com/yaooxu/image-bed/raw/master/img/image-20200826154450793.png" alt="image-20200826154450793" style="zoom: 67%;" />
--- getScore.py
+++ getScore.py
@@ -1,43 +1,38 @@
-import re
-import bs4
 import time
 from bs4 import BeautifulSoup
-import urllib
-from functools import reduce
-import lxml
 import requests
 import argparse
 
 parser = argparse.ArgumentParser(description='添加学号以及密码')
 parser.add_argument('--ID', type=str, required=True,
                     help='学号')
-parser.add_argument('--password', type=str, required=True,
+parser.add_argument('--password', type=str, required=False,
                     help='密码')
-parser.add_argument('--freq', type=int, default=60,
-                    help='查询频率，默认为60s')
+parser.add_argument('--freq', type=int, default=-1,
+                    help='查询频率，默认为-1，即只查询一次')
 args = parser.parse_args()
 
-url = 'https://login.bit.edu.cn/cas/login?service=http%3A%2F%2Fjwms.bit.edu.cn%2F'
+url = 'https://webvpn.bit.edu.cn/login?cas_login=true'
+prefix = "https://webvpn.bit.edu.cn"
+
 datas = {
     'username': args.ID,
     'password': args.password,
-    # 'it'
-    # 'execution'
     '_eventId': 'submit',
     'rmShown': '1'
 }
+
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
     'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
-    'Referer': 'https://login.bit.edu.cn/cas/login?service=http%3A%2F%2Fjwms.bit.edu.cn%2F',
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0',
 }  # 定义头文件，伪装成浏览器
 
 
-def calAvg(Dict: dict):
+def getAvg(Dict: dict):
     semestersGpa = {}
     for value in Dict.values():
         if value['semester'] not in semestersGpa:
@@ -46,7 +41,7 @@ def calAvg(Dict: dict):
             semestersGpa[value['semester']]['credit_tot'] = 0
 
         semestersGpa[value['semester']]['score_tot'] += value['score'] * \
-            value['credit']
+                                                        value['credit']
         semestersGpa[value['semester']]['credit_tot'] += value['credit']
     print('\n')
     for key, value in semestersGpa.items():
@@ -63,28 +58,59 @@ def calAvg(Dict: dict):
 
 
 sessions = requests.session()
-res = sessions.get(url, headers=headers)
-soup = BeautifulSoup(res.text, 'lxml')
+response_login = sessions.get(url)
+
+# 重定向后的url
+url = response_login.url
+soup_login = BeautifulSoup(response_login.text, 'lxml')
+
 # 这两个是隐藏的post参数
-lt = soup.find('input', {'name': 'lt'}).get('value')
-execution = soup.find('input', {'name': 'execution'}).get('value')
+lt = soup_login.find('input', {'name': 'lt'}).get('value')
+execution = soup_login.find('input', {'name': 'execution'}).get('value')
 datas['lt'] = lt
 datas['execution'] = execution
-print(lt, execution)
-sessions.post(url, headers=headers, data=datas)  # 登录
 
-semDicts = {}
+print(lt, execution)
+response = sessions.post(url, data=datas)  # 登录
+
+# 登录后的主页面
+soup_main = BeautifulSoup(response.text, 'lxml')
+
+jwc_redirect = prefix + soup_main.find('div', {"data-detail": "jwc.bit.edu.cn"}).get(
+    'data-redirect')
+
+response_jwc = sessions.get(jwc_redirect)
+soup_jwc = BeautifulSoup(response_jwc.text, 'lxml')
+
+# 学生综合中心
+student_personal_center_url = soup_jwc.find('div', {'class': 'link'}).find_all('a')[0].get('href')
+student_personal_center = sessions.get(student_personal_center_url)
+soup_student_personal_center = BeautifulSoup(student_personal_center.text, 'lxml')
+
+# 课程成绩查询
+score_query_url = prefix + soup_student_personal_center.find('div', {'class': 'wap'}).find_all('a')[5].get('href')
+score_query = sessions.get(score_query_url)
+soup_score_query = BeautifulSoup(score_query.text, 'lxml')
+
+score_list_url = score_query_url[:score_query_url.rfind('/')] + '/cjcx_list'
+score_query_data = {
+    "kksj": "",
+    "kcxz": "",
+    "kcmc": "",
+    "xsfs": "all"
+}
+
+name2info = {}
 subjects = []
 times = 1
-scoreList_url = 'http://jwms.bit.edu.cn/jsxsd/kscj/cjcx_list'
 while (True):
     flag = 0  # 是否有更新
-    response = sessions.get(scoreList_url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    print('第{:d}次查询 状态: {:d}'.format(times, response.status_code))
-    # print(soup)
+    response_score_list = sessions.post(score_list_url, data=score_query_data)
+    soup_score_list = BeautifulSoup(response_score_list.text, 'lxml')
+    print('第{:d}次查询 状态: {:d}'.format(times, response_score_list.status_code))
+    # print(soup_login)
 
-    tr_tags = soup.find_all('tr')
+    tr_tags = soup_score_list.find_all('tr')
     for tr_tag in tr_tags[2:]:
         try:
             score = int(tr_tag.find('a').text)
@@ -96,7 +122,7 @@ while (True):
                     subjects.append(name)
                     flag = 1
                     print(semester, name, score, credit)
-                    semDicts[name] = {
+                    name2info[name] = {
                         'semester': semester,
                         'score': score,
                         'credit': credit
@@ -104,10 +130,14 @@ while (True):
         except Exception as e:
             print(e)
             pass
+
     if flag == 1 and times != 1:
         print('有新变更')
     if times == 1 or flag == 1:
-        calAvg(semDicts)
+        getAvg(name2info)
 
     times += 1
-    time.sleep(args.freq)
+    if (args.freq != -1):
+        time.sleep(args.freq)
+    else:
+        break
